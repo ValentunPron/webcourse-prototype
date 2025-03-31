2831965336ced8950d99eae9d46b5659260122c5
Valentyn <valentunpron@gmail.com>
2025-03-31 16:27:18 +0300

    add styles and configured tailwind

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl">It's my home</h1>
+      <section className="pink_container pattern">
+        <h1 className="heading">It's my home <br/> You are Freedom</h1>
+
+        <p className="sub-heading !max-w-3xl">Submit ideas, Vote on Pitches, and Get Noticed in Virtual Competitions</p>
+      </section>
     </>
   );
 }
